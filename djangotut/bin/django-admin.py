94f869136e52950864d852e13d55981bc67a74bf
caduong <anhduongtk97@gmail.com>
2019-09-18 23:07:30 +0700

    update

---
+++ djangotut/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/anhduong/Documents/Learning_Web/Django_python/djangotut/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
